b4fc4f9e95f274212ddf980c5575465e1ba0f8f7
TON THANAKORN <iamtoncashstation@gmail.com>
2020-08-30 12:41:10 +0700

    Add project files.

---
+++ Week 3-3/Source.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+		int a;
+		int b;
+		int c;
+		int d;
+		printf("a = ");
+		scanf_s("%d", &a);
+		printf("b = ");
+		scanf_s("%d", &b);
+		printf("c = ");
+		scanf_s("%d", &c);
+		printf("d = ");
+		scanf_s("%d", &d);
+
+		printf("a=%d b=%d c=%d d=%d", a, b, c, d);
+}
